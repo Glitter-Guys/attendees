9481119eb1700ee0faeb5efd958d5867b242178b
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-23 14:04:16 -0800

    Add function to format data and require the array of event ids

--- seedDB.js
+++ seedDB.js
@@ -1,14 +1,57 @@
-var data = require('./100UpcomingEvents.js')
-//seed data base file
-var getAllEventIds = function (data) {
-	var eventIds = [];
-	var parsedData = data
-	for (var i = 0; i < parsedData.length; i++) {
-		eventIds.push(parsedData[i].id);
-		console.log(parsedData[i].id);
+var data = require('./upcomingevents.js')
+var request = require('request');
+var eventIds = require('./seedEventIds.js');
+var mysql      = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+
+
+//function to parse data for event ids from meetup api
+// var getAllEventIds = function (data) {
+// 	var eventIds = [];
+// 	var parsedData = data
+// 	for (var i = 0; i < parsedData.length; i++) {
+// 		eventIds.push(parsedData[i].id);
+// 		console.log(parsedData[i].id);
+// 	}
+// 	return eventIds;
+// }
+
+//get fake user seed data from api 
+request('https://randomuser.me/api/?results=10', function (error, response, body) {	
+	var results = JSON.parse(body)
+  formatData(results.results, eventIds); 
+  // console.log('requested')
+});
+
+var formatData = function (data, eventIds) {
+	for (var i = 0; i < data.length; i++) {
+		var user = {};
+		user.id = data[i].login.username
+		user.firstName = data[i].name.first
+		user.lastName = data[i].name.last
+		user.photoURL = data[i].picture.large
+		user.events = []; 
+		for (var j = 0; j < Math.floor(Math.random()*15); j++) {
+			var randomIndex = Math.floor(Math.random()*107);
+			if (user.events.indexOf(eventIds[randomIndex]) === -1) {
+				user.events.push(eventIds[randomIndex])
+			}
+		}
+		console.log(user)
 	}
-	return eventIds;
 }
 
-getAllEventIds(data);
+//mySQL schema
+
+
+
+
 
