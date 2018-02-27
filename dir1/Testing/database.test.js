26a22bdcb3bb9b44eca36c0f90e999ba60cf3521
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-27 14:58:06 -0800

    Add testing function for seedDB

---
+++ dir1/Testing/database.test.js
@@ -0,0 +1,21 @@
+//unit tests with jest
+var database =  require('../seedDB.js')
+
+
+console.log('testing');
+//test data recieved from API
+
+	//test length
+	//test JSON
+	//test invalid get request
+
+
+
+//test format data funciton  
+	// test output data
+	// //
+
+
+
+
+//test if data is saved into databases
