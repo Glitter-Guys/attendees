0eb388a003e63de55a6aa81272f642070a77f500
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-26 18:21:19 -0800

    Insert eventIds into database--bug that only enters in one username for eventIds

--- seedDB.js
+++ seedDB.js
@@ -2,7 +2,6 @@ var data = require('./upcomingevents.js')
 var request = require('request');
 var eventIds = require('./seedEventIds.js');
 var mysql      = require('mysql');
-var async = require('async');
 
 
 var connection = mysql.createConnection({
@@ -60,7 +59,7 @@ request('https://randomuser.me/api/?results=5', function (error, response, body)
 // 				})
  
 
-// 		// insert events that user is attending into events_users table
+// // 		// insert events that user is attending into events_users table
 // 		var randomNumberOfEvents = Math.floor(Math.random()*20)
 // 		for (var j = 0; j < randomNumberOfEvents; j++) {
 // 			var randomIndex = Math.floor(Math.random() * 107);
@@ -80,34 +79,57 @@ request('https://randomuser.me/api/?results=5', function (error, response, body)
 
 
 
+
 var insertIntoDB = function (data, EventIds) {
   	for (var i = 0; i < data.length; i++) {
 
-		//format user data 
-		var user = {};
-		user.id = data[i].login.username
-		user.firstName = data[i].name.first
-		user.lastName = data[i].name.last
-		user.photoURL = data[i].picture.large
-	pool.getConnection(function (err, connection) {
-	var userQueryString = "insert into Users(personId, id, first, last, photoURL) values" + 
-		`('${i}', '${user.id}', '${user.firstName}', '${user.lastName}', '${user.photoURL}')`;
-  	connection.query(userQueryString, function (error) {
-    // And done with the connection.
-    connection.release();
+			//format user data 
+			var user = {};
+			user.id = data[i].login.username
+			user.firstName = data[i].name.first
+			user.lastName = data[i].name.last
+			user.photoURL = data[i].picture.large
 
-	    // Handle error after the release.
-	    if (error) throw error;
-	    console.log("saved")
-	    // Don't use the connection here, it has been returned to the pool.
-	  	});
-  	})
-  	}
+			//Open pooling connection and insert query	
+			pool.getConnection(function (err, connection) {
+			var userQueryString = "insert into Users(personId, id, first, last, photoURL) values" + 
+				`('${i}', '${user.id}', '${user.firstName}', '${user.lastName}', '${user.photoURL}')`;
+		  	connection.query(userQueryString, function (error) {
+			    // And done with the connection.
+			    connection.release();
+			    // Handle error after the release.
+			    if (error) throw error;
+			    console.log("saved")
+			    // Don't use the connection here, it has been returned to the pool.
+			  	});
+	  	})
+
+	  	//insert events that user is attending into events_users table
+	  	var randomNumberOfEvents = Math.floor(Math.random()*20)
+			for (var j = 0; j < randomNumberOfEvents; j++) {
+
+				pool.getConnection(function (err, connection) {
+				var randomIndex = Math.floor(Math.random() * 107);
+				var randomEventId = eventIds[randomIndex];
+				console.log(randomEventId);
+				var eventQueryString = "insert into Events_users(event_id, user_id) values" + 
+				`('${randomEventId}', '${user.id}')`;
+		  	connection.query(eventQueryString, function (error) {
+			    // And done with the connection.
+			    connection.release();
+			    // Handle error after the release.
+			    if (error) throw error;
+			    console.log(" event saved")
+			    // Don't use the connection here, it has been returned to the pool.
+			  	});
+			})
+		}
+	}
 };
 
 
 
 
-
+connection.end();
 
 
