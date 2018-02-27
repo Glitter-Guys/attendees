984e123abda34a2257c941d193561919e09bd956
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-27 14:46:41 -0800

    Add testing folder, file, and jest dependecies and script

---
+++ dir1/Testing/tests.js
@@ -0,0 +1,19 @@
+//unit tests with jest
+
+
+
+//test data recieved from API
+	//test length
+	//test JSON
+	//test invalid get request
+
+
+
+//test format data funciton  
+	// test output data
+	// //
+
+
+
+
+//test if data is saved into databases
