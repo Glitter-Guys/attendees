1c57096ce51e6567272f50d13c1676408b4468a8
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-28 13:18:39 -0800

    Refactor file structure to eliminate dir1

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+//Index jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.render(
+  <button>Get Data from API</button>,
+  document.getElementById('app')
+);
+
+
---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(8000, function() {
+  console.log('listening on port 8000');
+});
+
+
+
+app.get('/', (request, response) => {
+	
+})
