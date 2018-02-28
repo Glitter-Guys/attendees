96f3a065b2b7667bad41dabc349d425a2b284775
Sam Watson <Sam.Watson3775@gmail.com>
2018-02-28 15:43:53 -0800

    Add components folder and create attendees class

---
+++ client/src/components/app.jsx
@@ -0,0 +1,18 @@
+//app.jsx
+import react from 'react'
+import react from 'react-dom'
+
+
+class Attendees extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+    	return (
+    		<div> Attendees </div>
+    		)
+    }
+}
+
+export default Attendees;
