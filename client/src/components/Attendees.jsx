c49724495d539395d0817f4b0c5815b6542ab42c
Sam Watson <Sam.Watson3775@gmail.com>
2018-03-01 14:11:23 -0800

    Create and render attendees component to dom

---
+++ client/src/components/Attendees.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Attendee from './Attendee.jsx'
+
+class Attendees extends React.Component {
+  render () {
+    return (
+      <div>
+      <table>
+        <tbody>
+        <tr>
+          {this.props.eventUsers.map( (user, index) => {
+            if (index < 4) {
+              return <td><Attendee key={index} user={user} /></td> 
+            }
+          })}
+        </tr>
+        <tr>
+          {this.props.eventUsers.map( (user, index) => {
+            if (index > 4 && index < 9) {
+              return <td><Attendee key={4 + index} user={user} /></td> 
+            }
+          })}
+        </tr>
+        </tbody>
+      </table>
+      </div>
+      )
+  }
+}
+
+export default Attendees
